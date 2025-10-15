a16903cbd1a04e4c46c8a708f96879a2bf5bddcc
mohasarreb <mohasarreb2000@gmail.com>
2025-10-15 23:57:50 +0300

    Update Main.java

--- Project2_Shapes/src/Main.java
+++ Project2_Shapes/src/Main.java
@@ -1,8 +1,28 @@
+
 public class Main {
     public static void main(String[] args) {
-        Circle c = new Circle("Circle1", 5);
-        c.draw();
-        c.move();
-        c.resize(1.5);
+    
+        Circle circle = new Circle();
+
+    
+        circle.setRadius(10.0);
+        circle.moveTo(5, 7);
+
+    
+        System.out.println("🟢 معلومات الدائرة:");
+        System.out.println("الموقع: (" + circle.x + ", " + circle.y + ")");
+        System.out.println("نصف القطر: " + circle.getRadius());
+
+        
+        circle.move(3, 2);
+        System.out.println("بعد التحريك: (" + circle.x + ", " + circle.y + ")");
+
+    
+        circle.resize(1.5);
+        System.out.println("بعد التكبير: نصف القطر = " + circle.getRadius());
+
+        
+        circle.draw();
     }
 }
+
